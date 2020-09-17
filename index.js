92492151b81fc691be153bf02f150c786a40dda4
anaesposito <anaclaraesposito@gmail.com>
2020-09-17 20:05:35 -0300

    filtro de busqueda funcionando perfectamente

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// ACA ESTA DIVINAMENTE HECHO EL BOTON LIMPIAR
+// // ACA ESTA DIVINAMENTE HECHO EL BOTON LIMPIAR
 
 const limpiar = document.querySelector("#limpiar");
 const filtroRaiting = document.getElementsByClassName("check");
-const busqueda = document.querySelector("#busqueda");
+const filtroNombre = document.querySelector("#busqueda");
 const numeroQueMuestra = document.querySelector(".nro-mostrado");
 const totalNumerosMostrados = document.querySelector(".total-nros");
 
@@ -18,14 +18,35 @@ limpiar.onclick = () => {
 //empieza ocultar carrito//
 const clickCarrito = document.querySelector("#ocultar-carrito");
 const overlay = document.querySelector("#carrito");
+const botonCerrarCarrito = document.querySelector("#cerrar");
 
 clickCarrito.onclick = () => {
   overlay.classList.remove("ocultar");
 };
 
-window.onkeypress = (e) => {
-  if (e.keyCode === 32) {
-    overlay.classList.add("ocultar");
+botonCerrarCarrito.onclick = () => {
+  overlay.classList.add("ocultar");
+};
+
+// LISTO CARRITO APARECE Y DESAPRECE CON ESPACIO
+
+// LISTO FILTRO DE BUSQUEDA
+const tarjetas = document.getElementsByClassName("tarjeta");
+const filtroRating = document.getElementsByClassName("review-filter");
+
+busqueda.oninput = () => {
+  for (let tarjeta of tarjetas) {
+    let titulo = tarjeta.dataset.nombre;
+    let consulta = busqueda.value;
+    console.log(titulo);
+
+    if (titulo.includes(consulta)) {
+      tarjeta.classList.remove("ocultar");
+    } else {
+      tarjeta.classList.add("ocultar");
+    }
+    if (busqueda.value == "") {
+      tarjeta.classList.remove("ocultar");
+    }
   }
 };
-// LISTO CARRITO APARECE Y DESAPRECE CON ESPACIO
